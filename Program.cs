7e4c5e4a08ca4ba07fcad1d76afe2dbe0f8e6b33
kvsim <vanyamixeev2021@mail.ru>
2023-11-15 20:49:01 +0900

    Task 1

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Задача 1: Написать рекурсивный метод для вычисления: x^n / n!, где x - некоторое действительное число, а n - натуральное
+namespace Mikheev_Kruglov_Recursion_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение x: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Введите значение N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            double result = CalculateExpression(x, n);
+            Console.WriteLine($"{x}^{n} / {n}! = {result}");
+            Console.ReadLine();
+        }
+
+        static double CalculateExpression(double x, int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return (Math.Pow(x, n) / Factorial(n)) + CalculateExpression(x, n - 1);
+            }
+        }
+
+        static double Factorial(int num)
+        {
+            if (num == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return num * Factorial(num - 1);
+            }
+        }
+    }
+}
